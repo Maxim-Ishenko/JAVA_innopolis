2a590bc49158518e385603732276d4c0dd6907ca
Maksim Ischenko <Maksim.Ischenko@leroymerlin.ru>
2023-10-24 09:33:41 +0300

    Implemented the task 1 from homework1

---
+++ Homework1/src/ru/innopolis/java/homework1/Main.java
@@ -0,0 +1,7 @@
+package ru.innopolis.java.homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        NumbersBearer.presenter(1, 2, 3, 4);
+    }
+}
---
+++ Homework1/src/ru/innopolis/java/homework1/NumbersBearer.java
@@ -0,0 +1,8 @@
+package ru.innopolis.java.homework1;
+
+public class NumbersBearer {
+
+    public static void presenter(int a, int b, int c, int d) {
+        System.out.print(a + " " + b + " " + c + " " + d);
+    };
+}
