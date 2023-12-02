acf55e5e3646763788bcc87776f4451c54679c02
Maksim Ischenko <Maksim.Ischenko@leroymerlin.ru>
2023-12-02 10:41:49 +0300

    Implemented the additional task

--- Homeworks/src/ru/innopolis/java/homework09_incapsulation_modifiers/App.java
+++ Homeworks/src/ru/innopolis/java/homework09_incapsulation_modifiers/App.java
@@ -4,19 +4,24 @@ import ru.innopolis.java.homework09_incapsulation_modifiers.Car.Car;
 import ru.innopolis.java.homework09_incapsulation_modifiers.Car.PerformanceCar;
 import ru.innopolis.java.homework09_incapsulation_modifiers.Car.ShowCar;
 import ru.innopolis.java.homework09_incapsulation_modifiers.Garage.Garage;
-import ru.innopolis.java.homework09_incapsulation_modifiers.Race.CasualRace;
-import ru.innopolis.java.homework09_incapsulation_modifiers.Race.DragRace;
-import ru.innopolis.java.homework09_incapsulation_modifiers.Race.DriftRace;
-import ru.innopolis.java.homework09_incapsulation_modifiers.Race.Race;
+import ru.innopolis.java.homework09_incapsulation_modifiers.Race.*;
+
+// Пояснения по дополнительному заданию
+// В условии дополнительного задания не оговорено -на каком уровне реализовать чтение и запись файла.
+// Так как, при желании, информация о времени и количестве кругов может храниться в разных файлах и логика обработки может отличаться,
+// я реализовал данную логику в обоих классах - и TimeLimitRace и CircuitRace, без выноса в отдельный класс.
+// По этой же причине запись информации в файл также реализовал только в дополнительных классах
+
 
 public class App {
+    static String outputFilePath = "src/ru/innopolis/java/homework09_incapsulation_modifiers/data/output.txt";
     public static void main(String[] args) throws Exception {
         Car opel = new Car("opel", "34fwsf", 1987, 67, 1, 54, 33);
         PerformanceCar mockvich = new PerformanceCar("mockvich", "412", 1980, 50, 78, 5, 45);
         PerformanceCar mockvichWithAddons = new PerformanceCar("mockvich", "412", 1980, 50, 78, 5, 45, new String[]{"as", "aas"});
         ShowCar niva = new ShowCar("lada", "niva", 1990, 76, 1,7,20);
         ShowCar nivaWithStars = new ShowCar("lada", "niva", 1990, 76, 1,7,20, 7);
-
+// 1
         System.out.println("Cars: {" + "\n" +
             "opel: " + opel + ";\n" +
             "mockvich: " + mockvich + ";\n" +
@@ -42,5 +47,13 @@ public class App {
         System.out.println("Garages: {" + "\n" +
                 "emptyGarage: " + emptyGarage + ";\n" +
                 "garage: " +  garage + ";\n" + "}");
+
+// 2
+//        TimeLimitRace timeLimitRace = new TimeLimitRace(140, "Babrujsk-NewYork", 140, new Car[]{opel, niva});
+//        timeLimitRace.setWriteTheResultsToTheFile(timeLimitRace.toString(), outputFilePath);
+//        CircuitRace circuitRace = new CircuitRace(1870, "Minsk-Pekin", 14, new Car[]{opel, niva});
+//        circuitRace.setWriteTheResultsToTheFile(circuitRace.toString(), outputFilePath);
+//        System.out.println("timeLimitRace " + timeLimitRace);
+//        System.out.println("circuitRace " + circuitRace);
     }
 }
---
+++ Homeworks/src/ru/innopolis/java/homework09_incapsulation_modifiers/Race/TimeLimitRace.java
@@ -0,0 +1,136 @@
+package ru.innopolis.java.homework09_incapsulation_modifiers.Race;
+
+import ru.innopolis.java.homework09_incapsulation_modifiers.Car.Car;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class TimeLimitRace extends Race {
+    private final String PARAMS_SEPARATOR = ",";
+    private final String PARAMS_NAME_VALUE_SEPARATOR = ":";
+    private final Integer FIRST_ELEMENT_OF_ARR_INDEX = 0;
+    private final Integer SECOND_ELEMENT_OF_ARR_INDEX = 1;
+    private final String GOLD_TIME_PARAM_NAME = "goldTime";
+    private final String INPUT_FILE_PATH = "src/ru/innopolis/java/homework09_incapsulation_modifiers/data/input.txt";
+
+    private Integer goldTime;
+
+    public Integer getGoldTime() {
+        return goldTime;
+    }
+
+    public void setGoldTime(Integer goldTime) throws Exception {
+        if (goldTime <= 0) {
+            throw new Exception("Значение времени должно быть положительным числом!");
+        }
+
+        this.goldTime = goldTime;
+    }
+
+    public TimeLimitRace() {
+        super();
+    }
+    public TimeLimitRace(
+            Integer distance,
+            String route,
+            Integer prize,
+            Car[] participants,
+            Integer goldTime
+    ) throws Exception {
+        super(
+                distance,
+                route,
+                prize,
+                participants
+        );
+
+        this.setGoldTime(goldTime);
+    }
+    public TimeLimitRace(
+            Integer distance,
+            String route,
+            Integer prize,
+            Car[] participants
+    ) throws Exception {
+        super(
+                distance,
+                route,
+                prize,
+                participants
+        );
+
+        String paramsStringFromTheFile = this.setParamsFromTheFile();
+        Integer goldTimeFromTheFile = Integer.valueOf(this.setFieldValueFromTheString(paramsStringFromTheFile));
+
+        this.setGoldTime(goldTimeFromTheFile);
+    }
+
+    public String setParamsFromTheFile() {
+        StringBuilder resultString = new StringBuilder();
+
+        try (BufferedReader bufferReader = new BufferedReader(new FileReader(this.INPUT_FILE_PATH))) {
+            String textLine;
+
+            while((textLine = bufferReader.readLine()) != null) {
+                resultString.append(textLine);
+                resultString.append("\n");
+            }
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    return resultString.toString().trim();
+    }
+    public String setFieldValueFromTheString (String fieldsStringFromTheFile) throws Exception {
+        StringBuilder targetValue = new StringBuilder();
+
+        for (String fieldEntity: fieldsStringFromTheFile.trim().split(this.PARAMS_SEPARATOR)) {
+            String paramName = fieldEntity.trim().split(this.PARAMS_NAME_VALUE_SEPARATOR)[this.FIRST_ELEMENT_OF_ARR_INDEX];
+            String paramVal = fieldEntity.trim().split(this.PARAMS_NAME_VALUE_SEPARATOR)[this.SECOND_ELEMENT_OF_ARR_INDEX];
+
+            if (Objects.equals(paramName, GOLD_TIME_PARAM_NAME)) targetValue.append(paramVal);
+        }
+
+        return targetValue.toString().trim();
+    }
+    public void setWriteTheResultsToTheFile(String resultString, String fileName) {
+        try {
+            BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(fileName, true));
+            bufferWriter.append("\n");
+            bufferWriter.append(resultString);
+
+            bufferWriter.close();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TimeLimitRace{" +
+            "distance=" + this.getDistance() +
+            ", route='" + this.getRoute() + '\'' +
+            ", prize=" + this.getPrize() +
+            ", goldTime=" + this.getGoldTime() +
+            ", participants=" + Arrays.toString(this.getParticipants()) +
+            '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TimeLimitRace that = (TimeLimitRace) o;
+        return Objects.equals(FIRST_ELEMENT_OF_ARR_INDEX, that.FIRST_ELEMENT_OF_ARR_INDEX)
+                && Objects.equals(SECOND_ELEMENT_OF_ARR_INDEX, that.SECOND_ELEMENT_OF_ARR_INDEX)
+                && Objects.equals(getGoldTime(), that.getGoldTime());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), PARAMS_SEPARATOR, PARAMS_NAME_VALUE_SEPARATOR, FIRST_ELEMENT_OF_ARR_INDEX, SECOND_ELEMENT_OF_ARR_INDEX, GOLD_TIME_PARAM_NAME, INPUT_FILE_PATH, getGoldTime());
+    }
+}
