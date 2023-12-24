541719bbcdb43720ed213f22da287b891ea9e0be
Maksim Ischenko <Maksim.Ischenko@leroymerlin.ru>
2023-12-24 08:25:24 +0300

    Fixed in accordance with the comments from code review

--- Homeworks/src/ru/innopolis/java/homework10_addition/App.java
+++ Homeworks/src/ru/innopolis/java/homework10_addition/App.java
@@ -3,15 +3,15 @@ package ru.innopolis.java.homework10_addition;
 public class App {
     public static void main(String[] args) {
         Pair<Integer, String> pair = Pair.of(1, "hello");
-        Integer i = pair.getFirst(); // 1
-        String s = pair.getSecond(); // "hello"
+        Integer number = pair.getFirst(); // 1
+        String string = pair.getSecond(); // "hello"
 
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
 
-        System.out.println("i: " + i);
-        System.out.println("s: " + s);
+        System.out.println("number: " + number);
+        System.out.println("string: " + string);
         System.out.println("mustBeTrue: " + mustBeTrue);
         System.out.println("mustAlsoBeTrue: " + mustAlsoBeTrue);
     }
