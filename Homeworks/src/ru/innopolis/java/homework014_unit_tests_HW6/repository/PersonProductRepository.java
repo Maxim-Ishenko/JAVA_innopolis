eb15c19784154af53614202fb4e9587609c71218
Maksim Ischenko <Maksim.Ischenko@leroymerlin.ru>
2024-01-26 09:47:18 +0300

    removed extra code

--- Homeworks/src/ru/innopolis/java/homework014_unit_tests_HW6/repository/PersonProductRepository.java
+++ Homeworks/src/ru/innopolis/java/homework014_unit_tests_HW6/repository/PersonProductRepository.java
@@ -6,8 +6,8 @@ import ru.innopolis.java.homework014_unit_tests_HW6.model.Product;
 public interface PersonProductRepository {
     Person createNewPerson(String name, double moneyAmount);
     Product createNewProduct(String name, double coast);
-    public String getPersonName(String name);
-    public Double getPersonMoneyAmount(Double moneyAmount);
-    public String getProductName(String productName);
-    public Double getProductCoast(Double coast);
+    String getPersonName(String name);
+    Double getPersonMoneyAmount(Double moneyAmount);
+    String getProductName(String productName);
+    Double getProductCoast(Double coast);
 }
